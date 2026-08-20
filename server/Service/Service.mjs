110356dac563069668ecd130a389b58b84819ad4
Mohammedthariq <mohammedthariq20@gmail.com>
2026-08-20 13:50:58 +0530

    AI Based Project Recommendation

--- server/Service/Service.mjs
+++ server/Service/Service.mjs
@@ -67,7 +67,7 @@ STRICT RULES:
 `;
     
     const response = await getClient().chat.completions.create({
-        model: 'llama-3.1-8b-instant',
+        model: 'meta-llama/llama-prompt-guard-2-86m',
         response_format: { type: 'json_object' },
         messages: [{ role: 'user', content: prompt}]
     });
